549c4b7bb1775552f68c38405e09ea0c42f1c8d7
Haruhisa Ishida <haruhisa.ishida@gmail.com>
2013-03-19 02:11:44 +0900

    フラグの生成/存在チェックのモジュール追加

---
+++ src/resources/flag.py
@@ -0,0 +1,31 @@
+# -*- coding: utf8 -*-
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      glycine
+#
+# Created:     14/01/2013
+# Copyright:   (c) glycine 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+import os
+
+class filenames:
+	# 字幕抽出のflagファイル名
+	SUBTITLE = "__subtitle"
+
+def gen_flag_file( flag_filename, dir_path ):
+	flag_filepath = os.path.join( dir_path, flag_filename )
+	flag_file = open( flag_filepath, "w")
+	flag_file.write("")
+	flag_file.close()
+	return
+
+def check_flag_existence( flag_filename, dir_path):
+	flag_filepath = os.path.join( dir_path, flag_filename)
+	if (os.path.isfile( flag_filepath )):
+		return True
+	return False
